25988af9387fbf6d73213525d5125f24c1492c02
Amani Magdi Farg <amanim.farag@gmail.com>
2024-04-01 13:38:09 +0200

    finalize database tables

--- work_order/models.py
+++ work_order/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+class LeadTech(models.Model):
+    name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
+
+class ServiceType(models.Model):
+    name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
+
+class Payment(models.Model):
+    payment_type = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.payment_type
+
+class Techs(models.Model):
+    techs = models.IntegerField()
+    lbr_rate = models.IntegerField()
+    
+class ServiceRequest(models.Model):
+    wo = models.CharField(max_length=100, unique=True)
+    district = models.CharField(max_length=100)
+    lead_tech = models.ForeignKey(LeadTech, on_delete=models.CASCADE)
+    service_type = models.ForeignKey(ServiceType, on_delete=models.CASCADE)
+    rush = models.BooleanField(null=True)
+    req_date = models.DateField()
+    work_date = models.DateField()
+    techs = models.IntegerField()
+    wty_lbr = models.BooleanField(null=True)
+    wty_parts = models.BooleanField(null=True)
+    lbr_hrs = models.FloatField(null=True)
+    parts_cost = models.FloatField()
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
+
+    @property
+    def lbr_rate(self):
+        #get lbr_rate from Techs table based on techs field
+        pass
+
+    @property
+    def work_day(self):
+        #get working day based on work_date
+        pass
+    
+
+
+
+
+
+
